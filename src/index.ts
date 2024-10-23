6bbe310274d7890f2e3ed82b9248ebeea27eaca3
Hamilton <hamil.jumbo@gmail.com>
2024-10-23 08:43:17 -0500

    Seguimiento del video hasat la conexion de la bd :(

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello, my name is Hamilton');
+//CONFIGURACION DEL SERVIDOR EXPRESS
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import compression from 'compression';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+    }));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+
+server.listen(8080, () => {                                                                     //ESTA FUNCION ES PARA QUE EL PUERTO 8080 
+    console.log('La aplicacion esta corriendo en http://localhost:8080');
+    });
+
+const  Mongo_URL = 'mongodb+srv://hamilton:hamilton@cluster0.6pcjw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.Promise = Promise;
+mongoose.connect(Mongo_URL);
+mongoose.connection.on( 'error', (error: Error) => console.log(error));
