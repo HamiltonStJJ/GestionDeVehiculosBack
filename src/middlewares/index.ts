8cb0b4a98a361c5c60e7f58ea6b4f10d6f3820fd
SharpBlade <yepezedwin760@gmail.com>
2024-10-23 17:57:22 -0500

    Middle para evitar eliminacion del usuario actual

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { get, identity, merge } from "lodash";
 import { getUserBySessionToken } from "../db/users";
+import e from "express";
 
 export const isAuthenticated = async (
   req: express.Request,
@@ -25,6 +26,33 @@ export const isAuthenticated = async (
     merge(req, { identity: existingUser });
 
     return next();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+    return;
+  }
+};
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUser = get(req, "identity._id") as string;
+
+    if (!currentUser) {
+      res.sendStatus(403);
+      return;
+    }
+
+    if (currentUser.toString() !== id) {
+      res.sendStatus(403);
+      return;
+    }
+
+    next();
   } catch (error) {
     console.log(error);
     res.sendStatus(400);
