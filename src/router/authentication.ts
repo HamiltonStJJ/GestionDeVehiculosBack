afdeb4708fe714398e14c9263f02c06061263018
SharpBlade <yepezedwin760@gmail.com>
2024-10-23 22:30:11 -0500

    rutas para restablecer la contraseña

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authentication";
+import {
+  register,
+  login,
+  requestPasswordReset,
+  changePassword,
+} from "../controllers/authentication";
+import { isAuthenticated } from "../middlewares";
 
 export default (router: Router) => {
   router.post("/auth/register", register);
   router.post("/auth/login", login);
+  router.post("/auth/forgot", requestPasswordReset);
+  router.post("/auth/change", isAuthenticated, changePassword);
 };
