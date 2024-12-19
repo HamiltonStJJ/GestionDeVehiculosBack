92cc0caa77808035be7f41dcbff7507d9abf98c4
OscarJRM <oramirez0004@uta.edu.ec>
2024-12-19 03:37:58 -0500

    get all users for employee

--- src/router/users.ts
+++ src/router/users.ts
@@ -3,7 +3,7 @@ import { deleteUser, getAllUsers, getUser, updateUser } from "../controllers/use
 import { isAuthenticated, isAuthorized, isOwner } from "../middlewares";
 
 export default (router: express.Router) => {
-  router.get("/users", isAuthenticated, isAuthorized(["admin"]), getAllUsers);
+  router.get("/users", isAuthenticated, isAuthorized(["admin","empleado"]), getAllUsers);
   router.get("/users/:cedula", isAuthenticated, getUser);
   router.put("/users/:cedula", isAuthenticated, updateUser);
   router.delete("/users/:cedula", isAuthenticated, isOwner, deleteUser);
